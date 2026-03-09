bb621507897867e74821d02b39bdf0a5aa0dbcb2
Luciano Elish <lucianoelish05@gmail.com>
2026-03-09 14:25:16 -0400

    Added navbar and removed styles from app, changed about and home view and added placeholder text to views

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1 class="title">
+    This is the about page
+  </h1>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
+
 </style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">
+      Welcome to the home page!
+    </h1>
   </main>
 </template>
