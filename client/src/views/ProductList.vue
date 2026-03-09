cd6681eb63352370b7b4bd37cb059625088e2447
Luciano Elish <lucianoelish05@gmail.com>
2026-03-09 16:08:01 -0400

    Used dummy json product data to fill out product page

---
+++ client/src/views/ProductList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useProductsStore } from '@/stores/products'
+const products = useProductsStore()
+</script>
+
+<template>
+    <h1 class="title is-1"> Product List</h1>
+    <div class="grid is-col-min-10">
+      <div v-for="product in products.products" :key="product.id" class="box">
+        <img :src="product.thumbnail" alt="Product Image" class="image is-4by3">
+        <button class="button is-primary is-small add-button">
+          Add to Cart
+        </button>
+        <b>{{ product.title }}</b>
+        <p>{{ product.description }}</p>
+        <div class="price">{{ product.price }}</div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+.add-button {
+  float: right;
+  margin-top: .5em;
+}
+</style>
